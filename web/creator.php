0889a181cd70e2ae743fe492d84d67462dbcf44e
Antonio Tomac <antonio.tomac1989@gmail.com>
2016-07-18 19:34:51 +0200

    fomrt code

--- web/creator.php
+++ web/creator.php
@@ -1,4 +1,5 @@
 <?php
+
 //echo "<pre>";
 //print_r($_REQUEST);
 //echo "</pre>";
@@ -8,70 +9,70 @@ $backendHost = "http://10.11.12.3:8081";
 $type = $_REQUEST['type'];
 $groupId = $_REQUEST['groupId'];
 $ammount = urlencode($_REQUEST['ammount']);
-$date =  urlencode($_REQUEST['date']);
+$date = urlencode($_REQUEST['date']);
 if ($type == 'Payment') {
-	$userId = $_REQUEST['userId'];
-	$url = "$backendHost/groups/$groupId/payments";
-	$body = json_encode(array(
-		'userId' => $userId, 
-		'date' => $date, 
-		'ammount' => $ammount
-	));
-	$method = "POST";
+    $userId = $_REQUEST['userId'];
+    $url = "$backendHost/groups/$groupId/payments";
+    $body = json_encode(array(
+        'userId' => $userId,
+        'date' => $date,
+        'ammount' => $ammount
+    ));
+    $method = "POST";
 } else if ($type == 'Exchange') {
-	$fromUserId = $_REQUEST['fromUserId'];
-	$toUserId = $_REQUEST['toUserId'];
-	$url = "$backendHost/groups/$groupId/exchanges";
-	$body = json_encode(array(
-		'fromUserId' => $fromUserId, 
-		'toUserId' => $toUserId, 
-		'date' => $date, 
-		'ammount' => $ammount
-	));
-	$method = "POST";
+    $fromUserId = $_REQUEST['fromUserId'];
+    $toUserId = $_REQUEST['toUserId'];
+    $url = "$backendHost/groups/$groupId/exchanges";
+    $body = json_encode(array(
+        'fromUserId' => $fromUserId,
+        'toUserId' => $toUserId,
+        'date' => $date,
+        'ammount' => $ammount
+    ));
+    $method = "POST";
 } else if ($type == 'Expense') {
-	$url = "$backendHost/groups/$groupId/expenses";
-	$method = "POST";
-	$userIds = $_REQUEST['userIds'];
-	$name = $_REQUEST['expenseName'];
-	$custom = $_REQUEST['distributionType'] == 'custom';
-	$equalRatio = 1./count($userIds);
-	$userRatios = array();
-	foreach ($userIds as $userId) {
-		$userRatios[$userId] = $custom ? $_REQUEST["ratio_$userId"] : $equalRatio;
-	}
-	$body = json_encode(array(
-		'name' => $name, 
-		'date' => $date, 
-		'ammount' => $ammount,
-		'userRatios' => $userRatios
-	));
+    $url = "$backendHost/groups/$groupId/expenses";
+    $method = "POST";
+    $userIds = $_REQUEST['userIds'];
+    $name = $_REQUEST['expenseName'];
+    $custom = $_REQUEST['distributionType'] == 'custom';
+    $equalRatio = 1. / count($userIds);
+    $userRatios = array();
+    foreach ($userIds as $userId) {
+        $userRatios[$userId] = $custom ? $_REQUEST["ratio_$userId"] : $equalRatio;
+    }
+    $body = json_encode(array(
+        'name' => $name,
+        'date' => $date,
+        'ammount' => $ammount,
+        'userRatios' => $userRatios
+    ));
 } else if ($type == 'Income') {
-	$url = "$backendHost/groups/$groupId/incomes";
-	$method = "POST";
-	$userIds = $_REQUEST['userIds'];
-	$name = $_REQUEST['incomeName'];
-	$custom = $_REQUEST['distributionType'] == 'custom';
-	$equalRatio = 1./count($userIds);
-	$userRatios = array();
-	foreach ($userIds as $userId) {
-		$userRatios[$userId] = $custom ? $_REQUEST["ratio_$userId"] : $equalRatio;
-	}
-	$body = json_encode(array(
-		'name' => $name, 
-		'date' => $date, 
-		'ammount' => $ammount,
-		'userRatios' => $userRatios
-	));
+    $url = "$backendHost/groups/$groupId/incomes";
+    $method = "POST";
+    $userIds = $_REQUEST['userIds'];
+    $name = $_REQUEST['incomeName'];
+    $custom = $_REQUEST['distributionType'] == 'custom';
+    $equalRatio = 1. / count($userIds);
+    $userRatios = array();
+    foreach ($userIds as $userId) {
+        $userRatios[$userId] = $custom ? $_REQUEST["ratio_$userId"] : $equalRatio;
+    }
+    $body = json_encode(array(
+        'name' => $name,
+        'date' => $date,
+        'ammount' => $ammount,
+        'userRatios' => $userRatios
+    ));
 } else if ($type == 'Receivement') {
-	$userId = $_REQUEST['userId'];
-	$url = "$backendHost/groups/$groupId/receivements";
-	$body = json_encode(array(
-		'userId' => $userId, 
-		'date' => $date, 
-		'ammount' => $ammount
-	));
-	$method = "POST";
+    $userId = $_REQUEST['userId'];
+    $url = "$backendHost/groups/$groupId/receivements";
+    $body = json_encode(array(
+        'userId' => $userId,
+        'date' => $date,
+        'ammount' => $ammount
+    ));
+    $method = "POST";
 }
 
 $ch = curl_init();
@@ -79,10 +80,10 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json'                                                                               
-)); 
-$output = curl_exec($ch);       
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json'
+));
+$output = curl_exec($ch);
 curl_close($ch);
 
 //echo "output: $output\n";
